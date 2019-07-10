e689df181c995656c9773b6cd04bd15bec5ca961
Daniel Didah <didahdaniel@gmail.com>
2019-07-10 20:55:39 +0300

    Added option for changing view from list to grid

--- app/src/main/java/com/example/pricecompare/AdaptersHelper/RecycleGridAdapter.java
+++ app/src/main/java/com/example/pricecompare/AdaptersHelper/RecycleGridAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pricecompare.Products;
@@ -19,6 +20,13 @@ import java.util.ArrayList;
 
 public class RecycleGridAdapter extends RecyclerView.Adapter<RecycleGridAdapter.GridViewHolder>{
 
+    public static final int SPAN_COUNT_ONE = 1;
+    public static final int SPAN_COUNT_TWO = 2;
+
+    private static final int LIST_VIEW = 1;
+    private static final int GRID_VIEW = 2;
+
+    private GridLayoutManager mLayoutManager;
     ArrayList<Products> products;
 
     private RecycleGridAdapter.OnItemClickListener mListener;
@@ -34,7 +42,7 @@ public class RecycleGridAdapter extends RecyclerView.Adapter<RecycleGridAdapter.
     }
 
 
-    public static class GridViewHolder extends RecyclerView.ViewHolder{
+     class GridViewHolder extends RecyclerView.ViewHolder{
         public TextView productDescrption;
         public TextView NewPrice;
         public TextView OldPrice;
@@ -44,7 +52,7 @@ public class RecycleGridAdapter extends RecyclerView.Adapter<RecycleGridAdapter.
         public ImageView share;
         public ImageView save;
 
-        public GridViewHolder(@NonNull View itemView, final RecycleGridAdapter.OnItemClickListener listener) {
+        public GridViewHolder(@NonNull View itemView, final RecycleGridAdapter.OnItemClickListener listener, int viewType) {
             super(itemView);
 
             productDescrption=itemView.findViewById(R.id.product_description);
@@ -101,23 +109,27 @@ public class RecycleGridAdapter extends RecyclerView.Adapter<RecycleGridAdapter.
     }
 
 
-    public RecycleGridAdapter(ArrayList<Products> prod){
-
+    public RecycleGridAdapter(ArrayList<Products> prod,GridLayoutManager gridLayoutManager){
         products=prod;
+        mLayoutManager=gridLayoutManager;
     }
 
 
     @NonNull
     @Override
-    public RecycleGridAdapter.GridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item,parent,false);
-        RecycleGridAdapter.GridViewHolder listViewHolder=new RecycleGridAdapter.GridViewHolder(v,mListener);
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        if (viewType==LIST_VIEW){
+            v=LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        }else {
+            v= LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_item,parent,false);
+        }
 
-        return listViewHolder;
+        return new RecycleGridAdapter.GridViewHolder(v,mListener,viewType);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final RecycleGridAdapter.GridViewHolder holder, int position) {
+    public void onBindViewHolder(final GridViewHolder holder, int position) {
 
         Products currentProduct=products.get(position);
         holder.OldPrice.setText(currentProduct.getPriceOld());
@@ -140,6 +152,15 @@ public class RecycleGridAdapter extends RecyclerView.Adapter<RecycleGridAdapter.
 
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        int spanCount = mLayoutManager.getSpanCount();
+        if (spanCount == SPAN_COUNT_ONE) {
+            return LIST_VIEW;
+        } else {
+            return GRID_VIEW;
+        }
+    }
 
 
     @Override
