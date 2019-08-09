2e9e5d511691ad55a3a91b3701c028397c9ebe1b
Daniel Didah <didahdaniel@gmail.com>
2019-08-09 23:32:32 +0300

    added scanner Name Query

---
+++ app/src/main/java/com/example/pricecompare/WebScraper/ScannerQuery.java
@@ -0,0 +1,96 @@
+package com.example.pricecompare.WebScraper;
+
+import com.example.pricecompare.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class ScannerQuery {
+
+    public ScannerQuery(){
+
+    }
+
+    //this method
+    public static String getBarcodeName(String google, String databaseWeb){
+        String productName="";
+        Document Google=null;
+        Document DatabaseWeb=null;
+
+        try {
+            Google = Jsoup.connect(google).sslSocketFactory(socketFactory()).get();
+            DatabaseWeb = Jsoup.connect(databaseWeb).sslSocketFactory(socketFactory()).get();
+
+
+            String webName = null;
+            String googleName=Google.select("h3.LC20lb").text();
+
+            if (googleName.indexOf('-') != -1) {
+                googleName = googleName.replace(googleName.substring(googleName.indexOf('-') + 1), "");
+                googleName = googleName.replace("-", "");
+            }
+
+            for (Element row:DatabaseWeb.select("tr.even")){
+
+                for (int i=0;i<=2;i++){
+                    webName = row.select("td").text();
+                }
+
+                break;
+            }
+
+
+            if (webName!=null && !webName.isEmpty()){
+                productName=webName;
+
+            }else if (googleName!=null && !googleName.isEmpty() && webName==null && productName.isEmpty()){
+                productName=googleName;
+
+            }else{
+                productName="Product Name Not Found";
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return productName;
+    }
+
+
+
+    private static SSLSocketFactory socketFactory() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException("Failed to create a SSL socket factory", e);
+        }
+    }
+}
