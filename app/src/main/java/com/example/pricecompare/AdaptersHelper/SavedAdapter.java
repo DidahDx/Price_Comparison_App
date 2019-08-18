e258d318a108aa6aa54dd996f6d96908b0e6cbfd
Daniel Didah <didahdaniel@gmail.com>
2019-08-18 20:34:27 +0300

    minor update

--- app/src/main/java/com/example/pricecompare/AdaptersHelper/SavedAdapter.java
+++ app/src/main/java/com/example/pricecompare/AdaptersHelper/SavedAdapter.java
@@ -2,7 +2,6 @@ package com.example.pricecompare.AdaptersHelper;
 
 import android.graphics.Paint;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
